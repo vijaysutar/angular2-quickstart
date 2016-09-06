fb16272460994bb80dd79bb22b79e61784854ffa
vijaysutar <vjsutar21@gmail.com>
2016-09-06 09:54:45 +0530

    routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,15 +4,28 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent }   from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component';
+
+import { routing } from './app.routing';
+
 @NgModule({
   imports:      [ 
       BrowserModule,
-      FormsModule 
+      FormsModule,
+      routing 
     ],
   declarations: [ 
       AppComponent, 
-      HeroDetailComponent
+      HeroDetailComponent,
+      HeroesComponent,
+      DashboardComponent
     ],
+  providers: [
+    HeroService
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- app/dashboard.component.js
+++ app/dashboard.component.js
@@ -9,18 +9,17 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var AppComponent = (function () {
-    function AppComponent() {
-        this.title = 'Tour of heroes';
+var DashboardComponent = (function () {
+    function DashboardComponent() {
     }
-    AppComponent = __decorate([
+    DashboardComponent = __decorate([
         core_1.Component({
-            selecter: 'my-app',
-            template: "\n        <h1> {{title}} </h1>\n        <nav>\n            <a routerLink=\"/dashboard\">Dashboard</a>\n            <a routerLink=\"/heroes\">Heroes</a>\n        </nav>\n        \n        <router-outlet></router-outlet>\n    "
+            selector: 'my-dashboard',
+            templateUrl: 'app/dashboard.component.html'
         }), 
         __metadata('design:paramtypes', [])
-    ], AppComponent);
-    return AppComponent;
+    ], DashboardComponent);
+    return DashboardComponent;
 }());
-exports.AppComponent = AppComponent;
-//# sourceMappingURL=app.component.js.map
+exports.DashboardComponent = DashboardComponent;
+//# sourceMappingURL=dashboard.component.js.map
